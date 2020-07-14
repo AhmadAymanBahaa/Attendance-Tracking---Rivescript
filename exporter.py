4480ed1da8d2b8a8f3ee10dd50e800d8109c1fc8
Fatema Hassan <fhaman2010@gmail.com>
2020-07-14 18:13:30 +0200

    Saving to excel

--- exporter.py
+++ exporter.py
@@ -1,10 +1,21 @@
-import xlsxwriter
+import openpyxl
 
-workbook = xlsxwriter.Workbook('attendance.xlsx')
-worksheet = workbook.add_worksheet()
+workbookName = 'attendance.xlsx'
+workbook = openpyxl. load_workbook(workbookName)
 
-def export(name,id,time):
-    worksheet.write(0,0,name)
-    worksheet.write(0,1,id)
-    worksheet.write(0,2,time)
+
+def export(name, id, time):
+
+    for wks in workbook.worksheets:
+        wks.cell(1,1).value = "Name"
+        wks.cell(1,2).value = "ID"
+        wks.cell(1,3).value = "Time"
+
+        x = wks.max_row + 1
+        wks.cell(x, 1 ).value = name
+        wks.cell(x, 2).value = id
+        wks.cell(x, 3).value = time
+
+    workbook.save(workbookName)
     workbook.close()
+
